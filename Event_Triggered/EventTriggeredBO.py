c45b59b9e2d021c980ffbfe3e8a1d020be6317d4
Ben Miske <benmiske@gmail.com>
2026-03-10 17:40:31 -0400

    added comments and cleaned up a little code

--- Event_Triggered/EventTriggeredBO.py
+++ Event_Triggered/EventTriggeredBO.py
@@ -2,12 +2,16 @@ from skopt import gp_minimize
 from skopt.space import Real, Integer
 from EventTriggered import EventTriggeredParameters, EventTriggeredCommunication
 
+
+
+# This is the cost function currently used, which only minimizes error rate
 def findMinimumErrorRate(params):
     
     NUMBER_OF_INITIAL_TRIALS = 10               #Trials initially completed to get rough idea of error rate
     INITIAL_ERROR_RATE_NEEDED_TO_CONTINUE = 1.0 #Maximum error rate to continue trials
     NUMBER_OF_ADDITIONAL_TRIALS = 40            #Used in cases where error rate is less than above rate
 
+    # Unpackages parameters and then instantiates an EventTriggeredParameters varialble using the supplied values
     epsilon_zero, epsilon_one, sigma_offset, conf_time_steps, max_time_steps = params
     test_params = EventTriggeredParameters (
         epsilon_zero=epsilon_zero,
@@ -19,9 +23,11 @@ def findMinimumErrorRate(params):
     
     errors = 0
     time_steps = 0
+    # First runs a preliminary number of trials. If preforms poorly, doesn't waste time on further computation
     for _ in range(NUMBER_OF_INITIAL_TRIALS):
         errors += (EventTriggeredCommunication.EventTriggeredTest(test_params)).errors
         time_steps += (EventTriggeredCommunication.EventTriggeredTest(test_params)).time_steps
+    # If the error rate is belowed a designated threshold, we attempt more trials to get a better estimation of performance
     if (errors/NUMBER_OF_INITIAL_TRIALS) <= INITIAL_ERROR_RATE_NEEDED_TO_CONTINUE: 
         for _ in range (NUMBER_OF_ADDITIONAL_TRIALS):
             errors += (EventTriggeredCommunication.EventTriggeredTest(test_params)).errors
@@ -34,20 +40,23 @@ def findMinimumErrorRate(params):
         f"epsilon_zero={epsilon_zero:.2f}, epsilon_one={epsilon_one:.2f} sigma_offset={sigma_offset:.2f}, conf_time={conf_time_steps}, max_time_steps={max_time_steps} - "+
         f"Average Time Steps: {time_steps/(NUMBER_OF_INITIAL_TRIALS):.3f}. "+
         "Ending Early.")
+    # We return the final error rate to the optimization function.
     return (errors/(NUMBER_OF_INITIAL_TRIALS))
 
+
+
+# These are the parameters to be passed into the cost function. Formatted as:
+#   [type (Real/Integer)]([min_value], [max_value], name="[parameter_name]") 
 dimensions = [
     Real(0.01, 0.5, name="epsilon_zero"),
     Real(0.5, 2.0, name="epsilon_one"),
     Real(5.0, 20.0, name="sigma_offset"),
     Integer(10, 50, name="confidence_time_steps"),
     Integer(100, 300, name="max_time_steps"),
-#   Commented these params out since changing them can cause Lorenz to not converge. Would like to experiment with some variations though
-#    Real(-20.0, 20.0, name="sigma"),
-#    Real(-20.0, 20.0, name="beta"),
-#    Real(-20.0, 20.0, name="rho"),
 ]
 
+# This is the bayseian optimization function. 
+# Increasing n_initial_points will make it more likely to find a global min, while n_calls willhelp optimize whichever minimum it locates
 result = gp_minimize(findMinimumErrorRate, dimensions, n_calls = 120, n_initial_points=20)
 
 print(f"Optimal parameters: {result.x}")
